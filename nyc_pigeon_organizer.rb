3ce2f0af0d013d8bac8233609e0cf18dcf0f4fc9
Yahjaira Vasquez <vyahjaira23@gmail.com>
2020-03-07 22:40:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
+require 'pp'
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # binding.pry
+  name = {}
+  index = 0 
+  name << data.each {|names, index| puts names[index]}
+ 
 end
